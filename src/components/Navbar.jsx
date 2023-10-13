7f650658e40874869e3d6c1614d2ce7d46b28a5f
adnanamiruddin <adnan.amiruddin34@gmail.com>
2023-10-13 23:03:11 +0800

    fix: Change padding for nav item on component Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,52 +9,52 @@ const Navbar = () => {
   const [showCommunity, setShowCommunity] = useState(false);
 
   return (
-    <nav className="flex justify-betweens items-center fixed w-full backdrop-blur-sm z-50 px-4 py-5 md:py-4 md:px-12">
+    <nav className="flex justify-betweens items-center fixed w-full backdrop-blur-sm z-50 p-4 md:py-4 md:px-12">
       <Link href="/" className="text-xl tracking-wider md:text-3xl md:pl-4">
         <Image src={icon} alt="" width={100} height={100} />
       </Link>
 
-      <div className="flex w-full justify-end items-center gap-4 text-sm font-semibold md:gap-2">
+      <div className="flex w-full justify-end items-center gap-1 text-sm font-semibold md:gap-2">
         <Link
           href="/"
-          className="bg-transparent font-bold text-white hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
+          className="bg-transparent font-bold text-white p-2 hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
         >
           Home
         </Link>
         <Link
           href="/tools"
-          className="bg-transparent font-bold text-white hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
+          className="bg-transparent font-bold text-white p-2 hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
         >
           Tools
         </Link>
 
         <button
           onClick={() => setShowCommunity(!showCommunity)}
-          className="flex justify-center items-center gap-1 bg-transparent font-bold text-white hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
+          className="flex justify-center items-center gap-1 bg-transparent font-bold text-white p-2 hover:bg-[#D2FF3A] hover:text-black hover:border-[1px] hover:rounded-md md:px-4"
         >
           <p className="flex justify-center items-center">
             Community <MdArrowDropDown className="text-xl" />
           </p>
         </button>
 
-        {showCommunity ? (
-          <div className="absolute bg-slate-900 border-2 w-72 right-[8%] top-14 p-6 rounded-md duration-500 md:right-4">
-            <h3 className="text-sm text-justify">
-              Gabung Komunitas dan Temukan Informasi Seputar Tools AI
-            </h3>
-            <Link
-              href="https://t.me/+D5WFvWvF8EVkMTI1"
-              target="_blank"
-              rel="noopener"
-              className="mt-4 bg-[#D2FF3A] text-black font-bold p-2 rounded-md flex justify-evenly items-center text-sm"
-            >
-              Bergabung ke Komunitas
-              <LuArrowDownRight className="text-lg mt-1" />
-            </Link>
-          </div>
-        ) : (
-          ""
-        )}
+        <div
+          className={`absolute bg-slate-900 border-2 w-72 right-[5%] top-16 p-6 rounded-md duration-500 md:right-[3.8%] ${
+            showCommunity ? "opacity-100" : "opacity-0 pointer-events-none"
+          }}`}
+        >
+          <h3 className="text-sm text-justify">
+            Gabung Komunitas dan Temukan Informasi Seputar Tools AI
+          </h3>
+          <Link
+            href="https://t.me/+D5WFvWvF8EVkMTI1"
+            target="_blank"
+            rel="noopener"
+            className="mt-4 bg-[#D2FF3A] text-black font-bold p-2 rounded-md flex justify-evenly items-center text-sm"
+          >
+            Bergabung ke Komunitas
+            <LuArrowDownRight className="text-lg mt-1" />
+          </Link>
+        </div>
       </div>
     </nav>
   );
